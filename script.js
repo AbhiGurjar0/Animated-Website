e76610d02fed509d60cd0c7175fd036347d34641
Abhi Gurjar <abhishekbainsla190@gmail.com>
2024-12-31 23:58:11 +0530

    day6

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+function nav(){
+  var slide = document.querySelector('.drop');
+var slider = document.querySelector('.dropdown');
+slide.addEventListener("mouseenter",function(){
+  slider.style.display = "block";
+  gsap.from(".dropdown .b-1 h4",{
+    y:20,
+    opacity:0,
+    stagger:.1
+  })
+  gsap.from(".dropdown .b-2 h4",{
+    y:20,
+    opacity:0,
+    stagger:.1
+  })
+  gsap.from(".dropdown .b-3 h4",{
+    y:20,
+    opacity:0,
+    stagger:.1
+  })
+})
+slide.addEventListener("mouseleave",function(){
+  slider.style.display = "none";
+})
+
+
+}
+function firstpage(){
+  gsap.from(".text h1",{
+    y:20,
+    delay:.3,
+    stagger:.4,
+    opacity:0,
+    duration:1
+  })
+}
 function secondpage(){
     var second = document.querySelector("#second");
     var vid = second.childNodes[1];
@@ -33,4 +69,63 @@ vid.addEventListener("dblclick",function(elem){
 })
 
 }
+function thirdpage(){
+  var third = document.querySelectorAll('.upper ._box');
+
+third.forEach(elem => {
+  elem.addEventListener("mouseenter",function(){
+  elem.childNodes[5].childNodes[1].play();
+  })
+  elem.addEventListener("mouseleave",function(){
+    elem.childNodes[5].childNodes[1].load();
+  })
+  
+});
+}
+function fourthpage(){
+  var right = document.querySelectorAll('.relem');
+right.forEach(elem =>{
+  elem.addEventListener("mouseenter",function(){
+   gsap.to(elem.childNodes[7],{
+     scale:1,
+     opacity:1
+    })
+    elem.childNodes[5].style.opacity = "1",
+    elem.childNodes[5].childNodes[1].play()
+
+  })
+   elem.addEventListener("mouseleave",function(){
+    gsap.to(elem.childNodes[7],{
+     scale:0,
+     opacity:0
+    })
+    elem.childNodes[5].style.opacity = "0",
+    elem.childNodes[5].childNodes[1].load()
+
+  })
+    elem.addEventListener("mousemove",function(dets){
+      gsap.to(elem.childNodes[7],{
+        x: dets.x -elem.childNodes[7].getBoundingClientRect().x - 90,
+    y: dets.y - elem.childNodes[7].getBoundingClientRect().y - 120,
+       
+      })
+
+  })
+
+})
+}
+function locomotive(){
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+  });
+}
+
+nav();
+firstpage();
 secondpage();
+thirdpage();
+fourthpage();
+locomotive();
+
+
